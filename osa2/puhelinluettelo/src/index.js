b2a381a552297a8c1a3ff9b83a6c9c63f3721579
Elias Peteri <elias.peteri@hotmail.com>
2020-10-07 15:40:59 +0300

    osa2

--- osa2/puhelinluettelo/src/index.js
+++ osa2/puhelinluettelo/src/index.js
@@ -1,8 +1,21 @@
-// HOURS: 6h, 50 min
-
 import ReactDOM from 'react-dom'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import personService from './services/persons'
+import './index.css'
 
+// Notification component to handle informing the user of a successful operation
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+  return (
+    <div className="error">
+      {message}
+    </div>
+  )
+}
+
+// Person component includes information for a single object
 const Person = (props) => {
   return(
   <p key={props.id}>
@@ -10,7 +23,7 @@ const Person = (props) => {
   </p>
   )
 }
-
+// Phonebook component includes an array of Person components
 const Phonebook = (props) => {
   return(
     <div>
@@ -20,7 +33,7 @@ const Phonebook = (props) => {
     </div>
   )
 }
-
+// PersonForm component handles the form for sending new information to the JSON server
 const PersonForm = (props) => {
   return(
   <form onSubmit={props.addName}>
@@ -36,22 +49,29 @@ const PersonForm = (props) => {
 </form>
   )
 }
-
+// App component is the root of the application
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('')
-
   const [ newNumber, setNewNumber] = useState('')
+  const [ errorMessage, setErrorMessage] = useState('')
 
+// hook is used for useEffect below
+const hook = () => {
+  personService
+    .getAll()
+    .then(response => {
+      setPersons(response.data)
+    })
+}
+
+// using React Effects we can fetch data with axios from the JSON server
+useEffect(hook,[])
+
+// addName-function handles compiling the information from the PersonForm component to be compiled into a new object which will be pushed back into the JSON server
 const addName = (event) => {
   let duplicate = false
   event.preventDefault()
-  console.log('Added name...', event.target.value)
 
   const personObject = {
     name: newName,
@@ -62,35 +82,50 @@ const addName = (event) => {
   for (let index = 0; index < persons.length; index++) {
     if (newName === persons[index].name)
     { 
-      window.alert(`${newName} is already added to the phonebook`)
       duplicate = true
     }
   }
   if(!duplicate && newName !== "") {
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    duplicate = false
-  }
+    console.log(newName);
+    personService
+      .create(personObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Person ${newName} already exists`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        },5000)
+        setPersons(persons.filter(p => p.id !== id))
+      })
+}
 }
 
+// handleNameChange updates the state to match what's written into the name input box in PersonForm
 const handleNameChange = (event) => {
-  console.log(event.target.value);
   setNewName(event.target.value)
 }
 
+// handleNumberChange updates the state to match what's written into the number input box in PersonForm
 const handleNumberChange = (event) => {
-  // console.log(event.target.value);
   setNewNumber(event.target.value)
 }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMessage}/>
+      <h2>add a new</h2>
       <PersonForm name={newName} number={newNumber} addName={addName} hnuc={handleNumberChange} hnac={handleNameChange}/>
       <h2>Numbers</h2>
-      <Phonebook persons={persons} />
+      <Phonebook persons={persons}/>
     </div>
   )
 }
+
 export default App
 ReactDOM.render(<App />,document.getElementById('root'))
