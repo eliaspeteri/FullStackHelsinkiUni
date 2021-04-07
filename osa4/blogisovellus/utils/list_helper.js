9fb01a731ba7a9508765149f57f7ff151e889fcc
Elias Peteri <elias.peteri@hotmail.com>
2021-04-07 11:44:26 +0300

    4.5

--- osa4/blogisovellus/utils/list_helper.js
+++ osa4/blogisovellus/utils/list_helper.js
@@ -10,7 +10,23 @@ const totalLikes = (blogs) => {
     return likesSum;
 };
 
+const favoriteBlog = (blogs) => {
+    let favoriteBlog = {
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+        __v: 0,
+    };
+    for (const blog of blogs) {
+        if (blog["likes"] >= favoriteBlog["likes"]) favoriteBlog = blog;
+    }
+    return favoriteBlog;
+};
+
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog,
 };
