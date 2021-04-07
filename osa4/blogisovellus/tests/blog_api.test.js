8f4baab2aed445653fc6e4b272544cb0a507885b
Elias Peteri <elias.peteri@hotmail.com>
2021-04-07 14:55:07 +0300

    4.8,4.15

--- osa4/blogisovellus/tests/blog_api.test.js
+++ osa4/blogisovellus/tests/blog_api.test.js
@@ -2,84 +2,104 @@ const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
 const api = supertest(app);
+const helper = require("./test_helper");
+const bcrypt = require("bcrypt");
 const Blog = require("../models/blog");
-const initialBlogs = [
-    {
-        _id: "5a422a851b54a676234d17f7",
-        title: "React patterns",
-        author: "Michael Chan",
-        url: "https://reactpatterns.com/",
-        likes: 7,
-        __v: 0,
-    },
-    {
-        _id: "5a422aa71b54a676234d17f8",
-        title: "Go To Statement Considered Harmful",
-        author: "Edsger W. Dijkstra",
-        url:
-            "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-        likes: 5,
-        __v: 0,
-    },
-    {
-        _id: "5a422b3a1b54a676234d17f9",
-        title: "Canonical string reduction",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-        likes: 12,
-        __v: 0,
-    },
-    {
-        _id: "5a422b891b54a676234d17fa",
-        title: "First class tests",
-        author: "Robert C. Martin",
-        url:
-            "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-        likes: 10,
-        __v: 0,
-    },
-    {
-        _id: "5a422ba71b54a676234d17fb",
-        title: "TDD harms architecture",
-        author: "Robert C. Martin",
-        url:
-            "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-        likes: 0,
-        __v: 0,
-    },
-    {
-        _id: "5a422bc61b54a676234d17fc",
-        title: "Type wars",
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-        likes: 2,
-        __v: 0,
-    },
-];
-beforeEach(async () => {
-    await Blog.deleteMany({});
-    for (const blog of initialBlogs) {
-        let blogObject = new Blog(blog);
-        await blogObject.save();
-    }
-});
-test("blogs are returned as json", async () => {
-    await api
-        .get("/api/blogs")
-        .expect(200)
-        .expect("Content-Type", /application\/json/);
+const User = require("../models/user");
+
+describe("when there are blogs in the database", () => {
+    beforeEach(async () => {
+        await Blog.deleteMany({});
+        for (const blog of helper.initialBlogs) {
+            let blogObject = new Blog(blog);
+            await blogObject.save();
+        }
+    });
+    test("blogs are returned as json", async () => {
+        await api
+            .get("/api/blogs")
+            .expect(200)
+            .expect("Content-Type", /application\/json/);
+    });
+    test("all blogs are returned", async () => {
+        const response = await api.get("/api/blogs");
+        expect(response.body).toHaveLength(helper.initialBlogs.length);
+    });
+    test("a specific blog is within the returned blogs", async () => {
+        const response = await api.get("/api/blogs");
+
+        const titles = response.body.map((r) => r.title);
+        expect(titles).toContainEqual("React patterns");
+    });
+    test("a valid blog can be added", async () => {
+        const blog = new Blog({
+            title: "Hello World",
+            author: "Elias",
+            url: "",
+        });
+
+        await api.post("/api/blogs", blog.save());
+
+        const blogsAtEnd = await helper.blogsInDb();
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 2);
+
+        const titles = blogsAtEnd.map((b) => b.title);
+        expect(titles).toContain("Hello World");
+    });
 });
 
-test("all blogs are returned", async () => {
-    const response = await api.get("/api/blogs");
-    expect(response.body).toHaveLength(initialBlogs.length);
-});
+describe("when there is initially one user at db", () => {
+    beforeEach(async () => {
+        await User.deleteMany({});
 
-test("a specific blog is within the returned blogs", async () => {
-    const response = await api.get("/api/blogs");
+        const passwordHash = await bcrypt.hash("sekret", 10);
+        const user = new User({ username: "root", passwordHash });
 
-    const titles = response.body.map((r) => r.title);
-    expect(titles).toContain("React patterns");
+        await user.save();
+    });
+
+    test("creation succeeds with a fresh username", async () => {
+        const usersAtStart = await helper.usersInDb();
+
+        const newUser = {
+            username: "elipet",
+            name: "Elias Peteri",
+            password: "salainen",
+        };
+
+        await api
+            .post("/api/users")
+            .send(newUser)
+            .expect(200)
+            .expect("Content-Type", /application\/json/);
+
+        const usersAtEnd = await helper.usersInDb();
+        expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
+
+        const usernames = usersAtEnd.map((u) => u.username);
+        expect(usernames).toContain(newUser.username);
+    });
+
+    test("creation fails with proper statuscode and message if username already taken", async () => {
+        const usersAtStart = await helper.usersInDb();
+
+        const newUser = {
+            username: "root",
+            name: "Superuser",
+            password: "salainen",
+        };
+
+        const result = await api
+            .post("/api/users")
+            .send(newUser)
+            .expect(400)
+            .expect("Content-Type", /application\/json/);
+
+        expect(result.body.error).toContain("`username` to be unique");
+
+        const usersAtEnd = await helper.usersInDb();
+        expect(usersAtEnd).toHaveLength(usersAtStart.length);
+    });
 });
 
 afterAll(() => {
